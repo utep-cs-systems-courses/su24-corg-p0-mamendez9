7be2f6e5a976097450213321b7953f610ee28a95
Martin Mendez Jr. <martinmendez@martins-air.lan>
2024-06-17 17:21:05 -0600

    UpdatedFiles

--- draw_chars.c
+++ draw_chars.c
@@ -14,3 +14,31 @@ void print_char_5x7(char c)
   }
   putchar('\n');
 }
+
+void print_char_11x16(short c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 11; row++) {
+    for (char col = 0; col < 16; col++) {
+      unsigned short rowBits = font_11x16[c][row];
+      unsigned short colMask = 1 << (15 - col);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
+
+void print_char_8x12(char c) {
+  c -= 0x20;
+  for (char row = 0; row < 8; row++) {
+    for (char col = 0; col < 12; col++) {
+      unsigned short rowBits = font_8x12[c][row];
+      unsigned short colMask = 1 << (11 - col);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
+
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a,) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -27,7 +27,12 @@ int main()
     case 'c':
       puts("You selected chars:");
       for (char c = 'a'; c < 'd'; c++)
-	print_char_5x7(c);
+	print_char_11x16(c);
+      break;
+    case 'a':
+      puts("you selected arrow:");
+      print_triangle(5, 7);
+      print_square(9, 7);
       break;
     case 'q':
       puts("Bye!");
